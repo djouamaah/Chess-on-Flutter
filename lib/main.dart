b7f8e5a252045785f9d0ad982a8f1dd9584212ee
Martin Fuentes <mefuentes@uninorte.edu.co>
2022-03-22 10:34:00 -0500

    Navigation between pages using the navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -31,15 +31,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,26 +38,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const _colorPrimary = Color(0xFF4E55F7);
+  static const _colorBPrimary = Color(0xFFEBEEFF);
+  static const _colorSecondary = Color(0xFF1D192B);
+  static const _colorBSecondary = Color(0xFF49454F);
+  static const _colorNeutral = Color(0xFFECF1F7);
+  int _currentIndex = 0;
+  final List _children = [
+    HomeWidget(),
+    AnalysisBoard(),
+    HomeTeacherWidget(),
+    ProfileWidget()
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Column(
-        children: <Widget>[
-          //Expanded(child: ProfileWidget())
-          //Expanded(child: MisCursosWidget())
-          //Expanded(child: HomeWidget())
-          Expanded(child: LoginWidget())
-        ],
+        children: <Widget>[Expanded(child: _children[_currentIndex])],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Color.fromRGBO(80, 87, 254, 1),
+        selectedItemColor: _colorPrimary,
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
