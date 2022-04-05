ba205bf249a8b8458e6ceaa13f8f3f787e832915
Donny <escolard@uninorte.edu.co>
2022-04-05 09:57:22 -0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: "MyApp"),
-      //home: LoginWidget()
+      //home: MyHomePage(title: "MyApp"),
+      home: LoginWidget()
     );
   }
 }
