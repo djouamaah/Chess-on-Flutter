10f0545bbe8acc19df7d4ce3c98012fea368fb9f
Helymar Acosta Escobar <helymar@outlook.com>
2022-04-18 01:43:51 -0500

    FIX api_key secrets

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDX1sRePBF4Mmz2E-QjGdLyr5f47u8JzsY")
+    GMSServices.provideAPIKey("API_KEY_MAPS_IOS")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
